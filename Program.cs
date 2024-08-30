a39949afb0e3dce80ba3e3e9f2e8cae2e34b59da
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-08-30 16:33:13 +0530

    NEW PROGRAM ADD FOR CALCULATOR

--- Program.cs
+++ Program.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace CPractice
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
+                int a = 5;
+                 int b = 10;
+
+            int adddin = Calculato.Add(a, b);
+
+            Console.WriteLine(adddin);
+
+            int sub = Calculato.substrac(a, b);
+
+            Console.WriteLine(sub);
+
+            int mal = Calculato.Maltiply(a, b);
+
+            Console.WriteLine(mal);
+
+
+            int div = Calculato.divide(a,b);
+
+            Console.WriteLine(div);
+
+            Console.ReadLine();
+           
+
         }
+    }
+
+    public static class Calculato
+    {
+
+        public static int Add(int a , int b ) => a + b;
+        public static int substrac(int a , int b ) => a - b;
+        public static int Maltiply(int a , int b ) => a * b;
+        public static int divide(int a , int b ) => a / b;
+
     }
 }
